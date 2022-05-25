5ac0edb5a302f0915ed370bae4e717a865e510a4
Deep-Ramani <deepramani8505@gmail.com>
2022-05-25 19:24:14 +0530

    Create Blog App

---
+++ components/Layout.jsx
@@ -0,0 +1,17 @@
+import React, { Children } from 'react'
+import Header from './Header'
+
+<Layout>
+  vdvdf
+</Layout>
+
+const Layout = () => {
+  return (
+    <>
+    <Header/>
+    {children}
+    </>
+  )
+}
+
+export default Layout
